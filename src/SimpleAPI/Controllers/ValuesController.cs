c7085a182a342db55a018832b0cd599a47b41f77
ducvanng <#maildungu@gmail.com>
2019-07-15 16:35:18 +0200

    Fixed Unit tests*

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Duc Power";
+            return "Duc";
         }
 
         // POST api/values
